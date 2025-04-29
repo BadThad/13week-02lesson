f596f634faf1e282f528cea39645c428cc5c10e5
Peter Fänge <pellefange@gmail.com>
2025-04-29 14:39:02 +0200

    created index.js with beginning of express server

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const { Pool } = pg;
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to this Node.js and PostgreSQL API!')
+});
+
+app.listen(3000, (req, res) => {
+    console.log("Server is running on port 3000.")
+})
